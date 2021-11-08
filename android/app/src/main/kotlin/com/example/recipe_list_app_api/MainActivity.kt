bfb2897c544d95b16f9bcdfab3102027ac891eb3
Amila1995 <amila.jayakody@outlook.com>
2021-11-08 09:01:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/recipe_list_app_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recipe_list_app_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:recipe_list_app_api/models/recipe.dart';
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "33d4e2d00cmshf11a13a6574bbd2p171622jsnf97e3af5ad2a",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
